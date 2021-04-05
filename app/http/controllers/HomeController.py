0e5aeaa3be877e2f9870fbc3ae25ad0cff9be29b
Daniel Vernon <ralliart2004@hotmail.com>
2021-04-04 20:04:42 -0500

    home page 90% done

--- app/http/controllers/HomeController.py
+++ app/http/controllers/HomeController.py
@@ -3,21 +3,15 @@
 from masonite.view import View
 from masonite.request import Request
 from masonite.controllers import Controller
+import datetime
 
 
 class HomeController(Controller):
     """Controller For Welcoming The User."""
 
     def show(self, view: View, request: Request):
-        """Show the welcome page.
-
-        Arguments:
-            view {masonite.view.View} -- The Masonite view class.
-            request {masonite.request.Request} -- The Masonite request class.
-
-        Returns:
-            masonite.view.View -- The Masonite view class.
-        """
+        # changes
 
         return view.render("pages/home", {
+            "cache_buster": datetime.datetime.now().strftime("%s"),
         })
