00c3c62888a8e558d32a9103e4d049a7255d2644
Daniel Vernon <ralliart2004@hotmail.com>
2021-04-11 15:27:46 -0500

    stats api

--- app/http/controllers/PageController.py
+++ app/http/controllers/PageController.py
@@ -4,6 +4,7 @@ from masonite.view import View
 from masonite.request import Request
 from masonite.controllers import Controller
 import datetime
+import requests
 
 
 class PageController(Controller):
@@ -12,10 +13,17 @@ class PageController(Controller):
     def home(self, view: View, request: Request):
         # change
 
+        stats_cdn_url = "https://d13wpvp4xr14sc.cloudfront.net"
+
+        response = requests.get(stats_cdn_url)
+
+        stats = response.json()
+
         return view.render("pages/home", {
             "cache_buster": datetime.datetime.now().strftime("%s"),
             "path": request.path,
-            "year": datetime.date.today().year
+            "year": datetime.date.today().year,
+            "stats": stats
         })
 
     def team(self, view: View, request: Request):
@@ -71,9 +79,11 @@ class PageController(Controller):
         job_data = [
             {"img_url": "/storage/static/images/Job-planet-1.png", "job_title": "Blockchain Developer", "job_desc": "We are looking for a full-time DApp/ DeFi Developer with Solidity skills who can work on a distributed (remote) team to continue the development and improvement of our platform, DeFi product suite, and technology strategy.", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
             {"img_url": "/storage/static/images/Job-planet-2.png", "job_title": "Marketing/Social Media Manager", "job_desc": "Our ideal person has proven experience identifying and engaging influencers, managing influencer outreach programs, and creating engaging, brand worthy content for all our social media platforms.", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
-            {"img_url": "/storage/static/images/Job-planet-3.png", "job_title": "HR/Payroll Specialist", "job_desc": "You demonstrate analytical and problem-solving skills with superb communication capability. This individual will need experience in payroll processing, benefits, and possess thorough knowledge of employment related laws and regulations", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
-            {"img_url": "/storage/static/images/Job-planet-1.png", "job_title": "Executive Assistant", "job_desc": "We are looking for someone who can assist the executive team with meeting scheduling, organization, official correspondence, and other tasks as they are requested. You should be able to demonstrate an ability to work fast and efficiently in fast-paced, high-stress environments.", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
-            {"img_url": "/storage/static/images/Job-planet-2.png", "job_title": "Community Manager", "job_desc": "We are looking for someone who is detail oriented, organized and can demonstrate the ability to work quickly, accurately and under pressure. Must be tech savvy. You should have a positive attitude and the desire to contribute to our team, be self-motivated, and able to work in a fast-paced, high-stress environments", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
+            {"img_url": "/storage/static/images/Job-planet-3.png", "job_title": "Marketing/Social Media Intern", "job_desc": "Our ideal person has proven experience identifying and engaging influencers, managing influencer outreach programs, and creating engaging, brand worthy content for all our social media platforms.", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
+            {"img_url": "/storage/static/images/Job-planet-2.png", "job_title": "HR/Payroll Specialist", "job_desc": "You demonstrate analytical and problem-solving skills with superb communication capability. This individual will need experience in payroll processing, benefits, and possess thorough knowledge of employment related laws and regulations", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
+            {"img_url": "/storage/static/images/Job-planet-3.png", "job_title": "Executive Assistant", "job_desc": "We are looking for someone who can assist the executive team with meeting scheduling, organization, official correspondence, and other tasks as they are requested. You should be able to demonstrate an ability to work fast and efficiently in fast-paced, high-stress environments.", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
+            {"img_url": "/storage/static/images/Job-planet-1.png", "job_title": "Community Manager", "job_desc": "We are looking for someone who is detail oriented, organized and can demonstrate the ability to work quickly, accurately and under pressure. Must be tech savvy. You should have a positive attitude and the desire to contribute to our team, be self-motivated, and able to work in a fast-paced, high-stress environments", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
+            {"img_url": "/storage/static/images/Job-planet-2.png", "job_title": "Community Intern", "job_desc": "We are looking for someone who is detail oriented, organized and can demonstrate the ability to work quickly, accurately and under pressure. Must be tech savvy. You should have a positive attitude and the desire to contribute to our team, be self-motivated, and able to work in a fast-paced, high-stress environments", "google_form_url": "https://safegalaxy.aidaform.com/Apply"},
         ]
 
         return view.render("pages/jobs", {
