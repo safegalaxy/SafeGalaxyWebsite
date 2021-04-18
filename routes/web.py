f97f031e89f6b03bcbc2860dcb8f1c4aa0df1b3b
daniel <three25ibmw@gmail.com>
2021-04-17 23:30:06 -0500

    api call only

--- routes/web.py
+++ routes/web.py
@@ -3,13 +3,13 @@
 from masonite.routes import Get, Post
 
 ROUTES = [
-    Get('/', 'PageController@home'),
-    Get('/team', 'PageController@team'),
-    Get('/whitepaper', 'PageController@whitepaper'),
-    Get('/how-to-buy', 'PageController@how_to_buy'),
-    Get('/jobs', 'PageController@jobs'),
-    Get('/tos', 'PageController@tos'),
-    Get('/policy', 'PageController@policy'),
+    # Get('/', 'PageController@home'),
+    # Get('/team', 'PageController@team'),
+    # Get('/whitepaper', 'PageController@whitepaper'),
+    # Get('/how-to-buy', 'PageController@how_to_buy'),
+    # Get('/jobs', 'PageController@jobs'),
+    # Get('/tos', 'PageController@tos'),
+    # Get('/policy', 'PageController@policy'),
 
     # API route
     Get('/public/stats', 'StatsController@stats'),
